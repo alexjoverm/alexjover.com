093de50a9402ea70f4460c44e91ab666aafb771f
Alex J <alexjovermorales@gmail.com>
2018-07-18 20:35:04 +0200

    first commit

---
+++ src/.vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  title: "Alex Jover",
+  description: "Web and JavaScript",
+  themeConfig: {
+    sidebar: ["/", "/ham", "/caca/"],
+    nav: ["Blog", "Courses", "About Me"]
+  },
+  configureWebpack: {
+    // resolve: {
+    //   alias: {
+    //     '@alias': 'path/to/some/dir'
+    //   }
+    // }
+  }
+};
---
+++ src/.vuepress/theme/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="navbar">
+    Navbar {{ $site.title }} - 
+    <span v-for="link in $site.themeConfig.nav"> {{link}} </span>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+@import "./styles/theme.scss";
+
+.navbar {
+  padding: 0.7rem 1.5rem;
+  line-height: $navbar-height - 1.4rem;
+  position: relative;
+  background: $accent-color;
+}
+</style>
